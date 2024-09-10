f3a9bcac2aa3bfea2cc0de91cf57f067c8e0557c
Sumanthxencia <148445847+Sumanthxencia@users.noreply.github.com>
2024-09-10 08:38:43 +0530

    Update deploy.sh

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -3,8 +3,8 @@ set -e
 
 ######################################
 ## Define Variables _ UPDATE VALUES
-BASE_NAME="<TBD>"
-LOCATION="northeurope"
+BASE_NAME="zycuspoc"
+LOCATION="eastus"
 ######################################
 
 ## Resource Group & Deployment
